5d0c1d7053c5b1f722cc7bb51746708000ba1495
Chris Lopes <chris.lopes@gmail.com>
2022-01-19 19:55:58 -0500

    Display appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from '/src/components/Appointment/Header';
+import Show from '/src/components/Appointment/Show';
+import Empty from '/src/components/Appointment/Empty';
 
 import './styles.scss';
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name} /> : <Empty />}
+    </article>
   );
 };
