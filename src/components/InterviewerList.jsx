16a66399660a4552f437477cc51a7fe5493874cf
Chris Lopes <chris.lopes@gmail.com>
2022-01-25 17:58:33 -0500

    Add prop-types to app

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import InterviewerListItem from './InterviewerListItem';
+import PropTypes from 'prop-types';
 
-import './InterviewerList.scss'
+import './InterviewerList.scss';
 
 export default function InterviewerList(props) {
-  const {interviewers, onChange, value} = props;
+  const { interviewers, onChange, value } = props;
 
-  const parsedInterviewers = 
+  const parsedInterviewers =
     Array.isArray(interviewers) &&
     interviewers.map((interviewer) => {
-      return <InterviewerListItem
-              key={interviewer.id}
-              name={interviewer.name}
-              avatar={interviewer.avatar}
-              selected={interviewer.id === value}
-              setInterviewer={() => onChange(interviewer.id)}
-              />
+      return (
+        <InterviewerListItem
+          key={interviewer.id}
+          name={interviewer.name}
+          avatar={interviewer.avatar}
+          selected={interviewer.id === value}
+          setInterviewer={() => onChange(interviewer.id)}
+        />
+      );
     });
 
   return (
-    <section className="interviewers">
-      <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list">
-        {parsedInterviewers}
-      </ul>
+    <section className='interviewers'>
+      <h4 className='interviewers__header text--light'>Interviewer</h4>
+      <ul className='interviewers__list'>{parsedInterviewers}</ul>
     </section>
   );
+}
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
 };
