f585a250191730c564f090fe12952cb64c15e36c
Chris Lopes <chris.lopes@gmail.com>
2022-01-26 15:09:05 -0500

    Update Cypress testing.

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -1,29 +1,32 @@
-import React from "react";
-import classNames from "classnames";
+import React from 'react';
+import classNames from 'classnames';
 
-import "./DayListItem.scss";
+import './DayListItem.scss';
 
 export default function DayListItem(props) {
-  const {name, spots, selected, setDay} = props;
-  const dayClass = classNames(
-    "day-list__item",
-    {"day-list__item--selected": selected,
-    "day-list__item--full": spots === 0
-    });
+  const { name, spots, selected, setDay } = props;
+  const dayClass = classNames('day-list__item', {
+    'day-list__item--selected': selected,
+    'day-list__item--full': spots === 0,
+  });
 
   const formatSpots = (spots) => {
     if (spots > 1) {
-      return <h3 className="text--light">{spots} spots remaining</h3>
+      return <h3 className='text--light'>{spots} spots remaining</h3>;
     } else if (spots === 1) {
-      return <h3 className="text--light">{spots} spot remaining</h3>
+      return <h3 className='text--light'>{spots} spot remaining</h3>;
     } else {
-      return <h3 className="text--light">no spots remaining</h3>
+      return <h3 className='text--light'>no spots remaining</h3>;
     }
-  }
+  };
 
   return (
-    <li onClick={() => setDay(name)} className={dayClass} selected={selected}>
-      <h2 className="text--regular">{name}</h2> 
+    <li
+      onClick={() => setDay(name)}
+      className={dayClass}
+      selected={selected}
+      data-testid={name}>
+      <h2 className='text--regular'>{name}</h2>
       {formatSpots(spots)}
     </li>
   );
